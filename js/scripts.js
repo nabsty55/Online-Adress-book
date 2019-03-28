b8af8400d6646bcc0fdba8f6433b031fb1bfedad
nabsty55 <nabsty55@gmail.com>
2019-03-28 16:22:58 +0300

    deleted one html file and now working with one file

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $('#form-button').click(function(){
+
+    $(".container").hide();
+    $(".container2").show();
+  })
+})
+
+/*  var firstName = $("fname").val();
+  var lastName = $("lname").val();
+  var contact1 ={firstname:firstName, lastname:"lastName"};
+  console.log(contact1);
+  $("#form-names ul").append("<li>"+contact1.firstname+"</li>");
+    $("#form-names ul").append("<li>firstname</li>");
+
+  })
+
+
+})*/
